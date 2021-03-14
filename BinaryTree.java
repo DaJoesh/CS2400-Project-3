ce99fe3ac1af06b0a1f1da169586190ee79f4530
rdrickards <78835468+rdrickards@users.noreply.github.com>
2021-03-14 16:17:14 -0700

    lkjahdfgh

--- BinaryTree.java
+++ BinaryTree.java
@@ -94,22 +94,12 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
     * prints (using post-order traversal) all nodes in the "whole" tree */
    public void postorderTraverse()
    {
-<<<<<<< HEAD
-=======
-      postorderTraverse(root);
->>>>>>> aac351b0845dee13ccaf6172cd3f4931997bc46f
    }
    
    /** A Recursive Method in the BinaryTree Class   
     * prints (using post-order traversal) all nodes in the subtree rooted at this node.*/
    private void postorderTraverse(BinaryNode<T> node)
    {
-      if(node!=null)
-      {
-         postorderTraverse(node.getLeftChild());
-         postorderTraverse(node.getRightChild());
-         System.out.println(node.getData());
-      }
    }
  
    /** The following calls postorderTraverse_binaryNodeMethod(), which is a recursive binaryNode class method   
@@ -170,10 +160,10 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
    @return  The number of nodes in the subtree rooted at this node. */
    private int getNumberOfNodes(BinaryNode<T> node)
    {
-      final int nodes = 8; 
-      int listOfNodes[] = new int[nodes];
-      ArrayList<Integer> adj[] = new ArrayList[nodes]; 
-      publiuc int numberOfNodes(int i, int j)  
+      //final int nodes = 8; 
+      //int listOfNodes[] = new int[nodes];
+      //ArrayList<Integer> adj[] = new ArrayList[nodes]; 
+      /*public int numberOfNodes(int i, int j)  
         listOfNodes[i] = 1; 
         for(Integer k: adj[i]) 
         { 
@@ -181,7 +171,7 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
                 continue; 
             numberOfNodes(k ,i); 
             listOfNodes[i] += listOfNodes[k]; 
-        }       
+        }  */     
       return 0;
    } // end getNumberOfNodes
    
