69c4d2c5860217dfa9565dcc70ce1cfb4433c9bf
BuildTools <unconfigured@null.spigotmc.org>
2021-03-21 19:31:30 -0700

    Finished DriverBT - Josh

--- DriverBT.java
+++ DriverBT.java
@@ -2,7 +2,7 @@ public class DriverBT
 {
 	public static void main(String[] args)
 	{
-		//first test
+		//1st Tree
 		System.out.println("1st Testing Example:");
 		BinaryTree<String> aTree = new BinaryTree<>();
 		createTree1(aTree);
@@ -25,7 +25,7 @@ public class DriverBT
 
 		System.out.println("==========================================");
 		System.out.println();
-		
+		//2nd Tree
 		System.out.println("2nd Testing Example:");
 		BinaryTree<String> aTree2 = new BinaryTree<>();
 		createTree2(aTree2);
@@ -47,28 +47,138 @@ public class DriverBT
 		System.out.println("(BinaryNode) # nodes of tree is " + aTree2.getNumberOfNodes_callBinaryNodeMethod());
 		
 		System.out.println("Done.");
-
-		System.out.println("3rd Testing Example:");
-		BinaryTree<String> aTree3 = new BinaryTree<>();
-		createUninitializedTree(aTree3);
+		//Root Only Tree
+		System.out.println("Root only Tree:");
+		BinaryTree<String> rootOnlyTree = new BinaryTree<>();
+		createRootOnly(rootOnlyTree);
 		
 		System.out.println("(binaryTree) post-order: ");
-		aTree3.postorderTraverse();
+		rootOnlyTree.postorderTraverse();
 		
 		System.out.println("(binaryNode) post-order: ");
-		aTree3.postorderTraverse_callBinaryNodeMethod();
+		rootOnlyTree.postorderTraverse_callBinaryNodeMethod();
 		
 		System.out.println();
 		
-		System.out.println("(BinaryTree) Height of tree is " + aTree3.getHeight());
-		System.out.println("(BinaryNode) Height of tree is " + aTree3.getHeight_callBinaryNodeMethod());
+		System.out.println("(BinaryTree) Height of tree is " + rootOnlyTree.getHeight());
+		System.out.println("(BinaryNode) Height of tree is " + rootOnlyTree.getHeight_callBinaryNodeMethod());
 		
 		System.out.println();
 		
-		System.out.println("(BinaryTree) # nodes of tree is " + aTree3.getNumberOfNodes());
-		System.out.println("(BinaryNode) # nodes of tree is " + aTree3.getNumberOfNodes_callBinaryNodeMethod());
+		System.out.println("(BinaryTree) # nodes of tree is " + rootOnlyTree.getNumberOfNodes());
+		System.out.println("(BinaryNode) # nodes of tree is " + rootOnlyTree.getNumberOfNodes_callBinaryNodeMethod());
 		
 		System.out.println("Done.");
+		//Left Only Tree
+		System.out.println("Left nodes only:");
+		BinaryTree<String> createLeftTree = new BinaryTree<>();
+		createLeftTree(createLeftTree);
+		
+		System.out.println("(binaryTree) post-order: ");
+		createLeftTree.postorderTraverse();
+		
+		System.out.println("(binaryNode) post-order: ");
+		createLeftTree.postorderTraverse_callBinaryNodeMethod();
+		
+		System.out.println();
+		
+		System.out.println("(BinaryTree) Height of tree is " + createLeftTree.getHeight());
+		System.out.println("(BinaryNode) Height of tree is " + createLeftTree.getHeight_callBinaryNodeMethod());
+		
+		System.out.println();
+		
+		System.out.println("(BinaryTree) # nodes of tree is " + createLeftTree.getNumberOfNodes());
+		System.out.println("(BinaryNode) # nodes of tree is " + createLeftTree.getNumberOfNodes_callBinaryNodeMethod());
+		
+		System.out.println("Done.");
+		//Right Only Tree
+		System.out.println("Right nodes only:");
+		BinaryTree<String> createRightTree = new BinaryTree<>();
+		createRightTree(createRightTree);
+		
+		System.out.println("(binaryTree) post-order: ");
+		createRightTree.postorderTraverse();
+		
+		System.out.println("(binaryNode) post-order: ");
+		createRightTree.postorderTraverse_callBinaryNodeMethod();
+		
+		System.out.println();
+		
+		System.out.println("(BinaryTree) Height of tree is " + createRightTree.getHeight());
+		System.out.println("(BinaryNode) Height of tree is " + createRightTree.getHeight_callBinaryNodeMethod());
+		
+		System.out.println();
+		
+		System.out.println("(BinaryTree) # nodes of tree is " + createRightTree.getNumberOfNodes());
+		System.out.println("(BinaryNode) # nodes of tree is " + createRightTree.getNumberOfNodes_callBinaryNodeMethod());
+		
+		System.out.println("Done.");
+		//3 Nodes Only Tree
+		System.out.println("3 nodes only:");
+		BinaryTree<String> threeNodesTree = new BinaryTree<>();
+		createUninitializedTree(threeNodesTree);
+		
+		System.out.println("(binaryTree) post-order: ");
+		threeNodesTree.postorderTraverse();
+		
+		System.out.println("(binaryNode) post-order: ");
+		threeNodesTree.postorderTraverse_callBinaryNodeMethod();
+		
+		System.out.println();
+		
+		System.out.println("(BinaryTree) Height of tree is " + threeNodesTree.getHeight());
+		System.out.println("(BinaryNode) Height of tree is " + threeNodesTree.getHeight_callBinaryNodeMethod());
+		
+		System.out.println();
+		
+		System.out.println("(BinaryTree) # nodes of tree is " + threeNodesTree.getNumberOfNodes());
+		System.out.println("(BinaryNode) # nodes of tree is " + threeNodesTree.getNumberOfNodes_callBinaryNodeMethod());
+		
+		System.out.println("Done.");
+		/*//Uninitialized Tree
+		System.out.println("Uninitialized Tree:");
+		BinaryTree<String> uninitializedTree = new BinaryTree<>();
+		createEmptyTree(uninitializedTree);
+		
+		System.out.println("(binaryTree) post-order: ");
+		uninitializedTree.postorderTraverse();
+		
+		System.out.println("(binaryNode) post-order: ");
+		uninitializedTree.postorderTraverse_callBinaryNodeMethod();
+		
+		System.out.println();
+		
+		System.out.println("(BinaryTree) Height of tree is " + uninitializedTree.getHeight());
+		System.out.println("(BinaryNode) Height of tree is " + uninitializedTree.getHeight_callBinaryNodeMethod());
+		
+		System.out.println();
+		
+		System.out.println("(BinaryTree) # nodes of tree is " + uninitializedTree.getNumberOfNodes());
+		System.out.println("(BinaryNode) # nodes of tree is " + uninitializedTree.getNumberOfNodes_callBinaryNodeMethod());
+		
+		System.out.println("Done.");
+		//Empty Tree
+		System.out.println("Empty Tree:");
+		BinaryTree<String> emptyTree = new BinaryTree<>();
+		createEmptyTree(emptyTree);
+		
+		System.out.println("(binaryTree) post-order: ");
+		emptyTree.postorderTraverse();
+		
+		System.out.println("(binaryNode) post-order: ");
+		emptyTree.postorderTraverse_callBinaryNodeMethod();
+		
+		System.out.println();
+		
+		System.out.println("(BinaryTree) Height of tree is " + emptyTree.getHeight());
+		System.out.println("(BinaryNode) Height of tree is " + emptyTree.getHeight_callBinaryNodeMethod());
+		
+		System.out.println();
+		
+		System.out.println("(BinaryTree) # nodes of tree is " + emptyTree.getNumberOfNodes());
+		System.out.println("(BinaryNode) # nodes of tree is " + emptyTree.getNumberOfNodes_callBinaryNodeMethod());
+		
+		System.out.println("Done.");*/
 	}  // end main
 
 	public static void createTree1(BinaryTree<String> tree)
